b01c36204335fb0e826151b8d0b5896b62d03d33
Эрнест Панков <er1k0@bk.ru>
2021-02-08 18:03:59 +0300

    Initial commit frontend

--- web/index.php
+++ web/index.php
@@ -3,7 +3,14 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Headers: *');
+    http_response_code ( 204 );
+    die;
+} else {
+    header('Access-Control-Allow-Origin: *');
+}
+//die;
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
