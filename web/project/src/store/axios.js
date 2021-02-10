0749463fde710ac4946c519dbdd78c91d4c61dac
Эрнест Панков <er1k0@bk.ru>
2021-02-10 14:20:38 +0300

    Правка url-а для запросов

--- web/project/src/store/axios.js
+++ web/project/src/store/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export  const AXIOS = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.VUE_APP_URL,
     headers: {
         ContentType: 'application/json',
     },
