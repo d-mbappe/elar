240bf62cfbecbb9a9bc62a2c6b509474e31aee0c
Эрнест Панков <er1k0@bk.ru>
2021-02-24 17:54:20 +0300

    Добавление плагина и кастомизация cropper

--- web/project/src/views/Main.vue
+++ web/project/src/views/Main.vue
@@ -1,214 +1,70 @@
 <template>
-<section>
+    <section>
+        <cropper
+                :src="img"
+                ref="cropper"
+                @change="onChange"
+                class="cropper"
+        />
+        <button @click="rotate(-90, )">left</button>
+        <button @click="rotate(90, )">right</button>
+    </section>
 
-
-    <div class="helper"></div><!--
-  	--><div class="drop display-inline align-center" @dragover.prevent @drop="onDrop">
-    <div class="custom__preview" v-if="!image">
-      <img class="custom__preview__logo" src="../assets/icons/avatar_logo.svg" alt="">
-        <p class="custom__preview__title">Перетащите фотографию сюда  или нажмите:</p>
-
-
-    </div>
-    <div  class="helper"></div><!--
-
-
-
---><label v-if="!image" class="btn display-inline">
-    Выберите файл
-    <input type="file" name="image" @change="onChange">
-
-
-</label><!--
-      --><div class="hidden display-inline align-center" v-else v-bind:class="{ 'image': true }">
-    <div class="image__wrap">
-        <img :src="image" alt="" class="img" />
-        <br>
-        <br>
-    </div>
-
-
-    <button class="btn__edit" type="file" @change="onChange"><img src="../assets/icons/edit_logo.svg" alt="">    <input type="file" name="image" @change="onChange">
-    </button>
-    <button class="btn__remove" @click="removeFile"><img src="../assets/icons/remove_logp.svg" alt=""></button>
-</div>
-    </label>
-
-</div>
-    <button class="btn" @click="removeFile">REMOVE</button>
-
-</section>
 </template>
 
-<script >
-    export default {
-        name: "Main",
+<script>
+    import { Cropper } from 'vue-advanced-cropper';
+    // import 'vue-advanced-cropper/dist/theme.classic.css';
+    import 'vue-advanced-cropper/dist/style.css';
 
+
+    export default {
+        components: {
+            Cropper,
+        },
         data() {
             return {
-                image: ''
+                // img: 'https://images.unsplash.com/photo-1590291409749-452efbe0d76c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+                img: 'https://images.pexels.com/photos/4323307/pexels-photo-4323307.jpeg',
 
-            }
+                result: {
+                    coordinates: null,
+                    image: null
+                }
+            };
         },
         methods: {
-            onDrop: function(e) {
-                e.stopPropagation();
-                e.preventDefault();
-                var files = e.dataTransfer.files;
-                this.createFile(files[0]);
+            onChange({ coordinates, image }) {
+                this.result = {
+                    coordinates,
+                    image
+                };
             },
-            onChange(e) {
-                var files = e.target.files;
-                this.createFile(files[0]);
-            },
-            createFile(file) {
-                if (!file.type.match('image.*')) {
-                    alert('Select an image');
-                    return;
-                }
-                var img = new Image();
-                var reader = new FileReader();
-                var vm = this;
 
-                reader.onload = function(e) {
-                    vm.image = e.target.result;
-                }
-                reader.readAsDataURL(file);
+            flip(x,y) {
+                this.$refs.cropper.flip(x,y);
             },
-            removeFile() {
-                this.image = '';
-            }
-        }
-    }
+            rotate(angle) {
+                this.$refs.cropper.rotate(angle);
+            },
+        },
+    };
 </script>
-
 <style lang="scss" scoped>
-    .btn {
-        margin-top: 15px;
-        background-color: white;
-        color: black;
-        border: 0;
-        cursor: pointer;
-        display: inline-block;
-        font-weight: bold;
-        padding: 3px 7px;
-        border-radius: 5px;
-        position: relative;
 
-        &__remove, &__edit {
-            position: absolute;
-            width: 40px;
-            height: 40px;
-            background-color: white;
-            border-radius: 50%;
+    /*@import '~vue-advanced-cropper/dist/theme.classic.css';*/
 
-            box-shadow: 0 0 10px rgba(0,0,0, 0.8);
 
-            input {
-                height: 100%;
-                top: 0;
-                position: absolute;
-                opacity: 0;
-                z-index: 1 !important;
-                width: 100%;
-                left: 0;
-                cursor: pointer;
-            }
-
-            img {
-                margin: auto;
-                cursor: pointer;
-            }
-
-        }
-
-        &__remove {
-            bottom: -20px;
-            right: 40px;
-        }
-
-        &__edit {
-            bottom: -20px;
-            left: 40px;
-        }
-    }
-
-    .btn:hover {
-        opacity: 0.8;
-    }
-
-    input[type="file"] {
-        position: absolute;
-        opacity: 0;
-        z-index: -1;
-    }
-
-    .align-center {
-        text-align: center;
-    }
-
-    .helper {
-        display: inline-block;
-        vertical-align: middle;
-        width: 0;
-    }
-
-    .hidden {
-        display: none !important;
-    }
-
-    .hidden {
-        height: 100%;
-        width: 100%;
-        display: inline-block !important;
-        position: relative;
-    }
-
-    .display-inline {
-        display: inline-block;
-        vertical-align: middle;
-    }
-
-    .image__wrap {
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-    }
-
-    .img {
-        object-fit: cover;
-        width: 100%;
-        height: 100%;
-
-        border-radius: 10px;
-        display: inline-block;
+    .cropper {
+        height: 600px;
+        background-color: black;
+        background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAA3NCSVQICAjb4U/gAAAABlBMVEXMzMz////TjRV2AAAACXBIWXMAAArrAAAK6wGCiw1aAAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M26LyyjAAAABFJREFUCJlj+M/AgBVhF/0PAH6/D/HkDxOGAAAAAElFTkSuQmCC);
+        /*opacity: .9;*/
 
     }
 
-    .drop {
-        margin: 30px;
-        height: 240px;
-        width: 185px;
-
-        background-color: #AEA4A1;
-        border-radius: 10px;
+    .сс {
+        background-color: black;
+        opacity: .2;
     }
-
-    .custom__preview {
-        margin-top: 40px;
-
-        color: white;
-        font-size: 14px;
-        line-height: 17px;
-
-        &__logo {
-            margin: 0 auto;
-        }
-
-        &__title {
-            margin-top: 25px;
-            padding: 0 40px;
-        }
-    }
-
 </style>
